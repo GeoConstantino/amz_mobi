a46f15e25d98095ba28f385802c3baa0a763c363
GeoConstantino <geovani.cpm@gmail.com>
2020-04-07 19:10:18 -0300

    refactoring the link schemma

--- main.py
+++ main.py
@@ -59,8 +59,14 @@ if __name__ == "__main__":
     chrome_options.add_argument("--headless")
     browser = webdriver.Chrome(options=chrome_options)
     
-    browser.get('https://www.amazon.com.br/s?i=digital-text&bbn=5308307011&rh=n%3A5308307011%2Cp_36%3A5560478011%2Cp_n_feature_browse-bin%3A6406077011%7C6406078011&s=popularity-rank&dc&page={}&language=en_US&_encoding=UTF8&fst=as%3Aoff&linkCode=sl2&linkId=02321018ed113bd4621621a252c9f986&qid=1586219653&rnid=19553430011&ref=sr_pg_2'.format(FIRST_PAGE_NUM))
-        
+    link_amazon = 'https://www.amazon.com.br/s?i=digital-text&bbn=5308307011&rh=n%3A5308307011%2Cp_36%3A5560478011%2Cp_n_feature_browse-bin%3A6406077011%7C6406078011&s=popularity-rank&dc&page={}&language=en_US&_encoding=UTF8&fst=as%3Aoff&linkCode=sl2&linkId=02321018ed113bd4621621a252c9f986&qid=1586219653&rnid=19553430011&ref=sr_pg_2'
+
+    print('currently seeing itens starting on: \n', link_amazon.format(FIRST_PAGE_NUM), '\n')
+    
+    browser.get(link_amazon.format(FIRST_PAGE_NUM))
+
+
+
     #TUTORIAL
 
     if os.path.isfile("cookies.pkl"):
@@ -89,9 +95,11 @@ if __name__ == "__main__":
 
     for page_number in range(FIRST_PAGE_NUM + 1, LAST_PAGE_NUM + 1):
 
-        print('--  PÁGINA {}  --\n\n'.format(page_number))
+        print('currently seeing itens starting on: \n', link_amazon.format(page_number), '\n' )
 
-        link = 'https://www.amazon.com.br/s?i=digital-text&bbn=5308307011&rh=n%3A5308307011%2Cp_36%3A5560478011%2Cp_n_feature_browse-bin%3A6406077011%7C6406078011&s=popularity-rank&dc&page={}&language=en_US&_encoding=UTF8&fst=as%3Aoff&linkCode=sl2&linkId=02321018ed113bd4621621a252c9f986&qid=1586219653&rnid=19553430011&ref=sr_pg_2'.format(page_number)
+        print('--  PAGE {}  --\n\n'.format(page_number))
+
+        link = link_amazon.format(page_number)
          
         browser.get(link)
 
